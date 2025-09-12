8490c4f4f997ae590c374cc3463b6c86cd0b6bfe
Yuria Loo <yuria.loo@comcast.net>
2025-09-12 14:26:39 -0700

    Add a temp query file

---
+++ application/queries.py
@@ -0,0 +1,14 @@
+# application/queries.py
+
+# Import the connection to the database
+from db_config import get_connection
+
+# Task 1: List which products we currently have in inventory
+# Task 2: Create new products
+# Task 3: Modify the amount of a particular product that we have in inventory
+# Task 4: Delete a product from inventory
+# Task 5: Get a list of the most popular products for a given time range
+# Task 6: Get a list of the least popular products for a given time range
+# Task 7: Get a list of users who haven't purchased something in a few months to send promotional emails to
+#(This should also include products that these users normally purchase)
+# Task 8: Get the inventory information for a specific product
